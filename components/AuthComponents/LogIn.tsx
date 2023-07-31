74b430f79a4470e612ff7eb9e25cd089e3c00013
SHWEDUS <krylov1243@gmail.com>
2023-07-31 23:26:05 +0500

    hotfix

--- components/AuthComponents/LogIn.tsx
+++ components/AuthComponents/LogIn.tsx
@@ -46,7 +46,7 @@ export const LogIn = () => {
                     </div>
                     {error&& (
                         <div className={styles.error}>
-                            {error.substring(0, error.length - 1)}
+                            {error.replace('e.', 'e').replace('ы.', 'ы')}
                         </div>
                     )}
                 </Form.Item>
