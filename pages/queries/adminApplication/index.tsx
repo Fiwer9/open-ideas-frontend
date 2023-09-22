7efc26cfac0f6bb1146328010455f60bc4a9cc0a
Maria <nikitina.ma2003@gmail.com>
2023-09-22 22:05:27 +0500

    Layout of the initiative card page in the admin panel

---
+++ pages/queries/adminApplication/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AdminApplicationCard } from "../../../components/QueryComponents/AdminApplicationCard";
+
+export default function Index() {
+  return (
+    <div>
+      <AdminApplicationCard />
+    </div>
+  )
+}
