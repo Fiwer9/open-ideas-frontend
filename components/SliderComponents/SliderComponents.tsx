cd6143fc3149e4048cd4abe892861ccac6db73d3
krylov gleb <krylov1243@gmail.com>
2023-11-20 14:10:07 +0500

    add direction step on sider

--- components/SliderComponents/SliderComponents.tsx
+++ components/SliderComponents/SliderComponents.tsx
@@ -5,7 +5,7 @@ import { Menu } from 'antd';
 import {
   BarChartOutlined,
   BulbOutlined, ReconciliationOutlined,
-  UserOutlined, SettingOutlined,
+  UserOutlined, SettingOutlined, PullRequestOutlined,
 } from '@ant-design/icons';
 import { ArrowBack } from './ArrowBack';
 import { ArrowNext } from './ArrowNext';
@@ -50,6 +50,7 @@ export const Slider = () => {
           <Menu.Item key={'1'} icon={<BulbOutlined />} onClick={() => router.push('/queries')}>Инициативы</Menu.Item>
           <Menu.Item key={'2'} icon={<UserOutlined />} onClick={() => router.push('/users')}>Пользователи</Menu.Item>
           <Menu.Item key={'3'} icon={<ReconciliationOutlined />} onClick={() => router.push('/organizations')}>Организации и отделы</Menu.Item>
+          <Menu.Item key={'3'} icon={<PullRequestOutlined />}>Направления</Menu.Item>
           <SubMenu key={'sub1'} icon={<BarChartOutlined />} title={'Аналитика'}>
             <Menu.Item key={'4'}>Графики</Menu.Item>
             <Menu.Item key={'5'}>Таблицы</Menu.Item>
