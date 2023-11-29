1ae5bff9a7c074f68ac1e7faf14b765d4e1fbe13
Мария <92936726+Rey-2903@users.noreply.github.com>
2023-11-29 16:40:11 +0500

    Correcting the path to the favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Favicon from "../components/FaviconComponents/Favicon";
+import React from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <Favicon />
+      <Head>
+        <title>Открытые идеи</title>
+        <link rel='shortcut icon' href={'./img/favicon.ico'} type="image/x-icon"/>
+      </Head>
       <body style={{margin: 0}}>
         <Main />
         <NextScript />
