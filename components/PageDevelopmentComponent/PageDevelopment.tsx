1a2a175bb2816d90cac0d9bf00919a8717a53d15
Мария <92936726+Rey-2903@users.noreply.github.com>
2024-01-12 16:09:00 +0500

    Adding a logo to a page in development

--- components/PageDevelopmentComponent/PageDevelopment.tsx
+++ components/PageDevelopmentComponent/PageDevelopment.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import styles from './styles/PageDevelopment.module.scss';
 import router from "next/router";
+import { Logo } from "../PicturesComponents/Logo";
 
 export const PageDevelopment = () => {
   return (
-    <div className={styles.container}>
-      <p className={styles.page}>Страница в разработке</p>
-      <button className={`${styles.btnBlue} ${styles.btnPage}`} onClick={() => router.push('/queries')}>Назад</button>
-    </div>
+    <>
+      <div className={styles.logotip}>
+        <Logo width={250} height={170} big={true} />
+      </div>
+      <div className={styles.container}>
+        <p className={styles.page}>Страница в разработке</p>
+        <button className={`${styles.btnBlue} ${styles.btnPage}`} onClick={() => router.push('/queries')}>Назад</button>
+      </div>
+    </>
   );
 };
