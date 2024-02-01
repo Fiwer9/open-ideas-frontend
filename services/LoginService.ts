9f8fe6c438c17571d400c9dde853661ca151ea4c
Крылов Глеб Иванович <krylov1243@gmail.com>
2024-02-01 18:42:16 +0500

    hotfix

--- services/LoginService.ts
+++ services/LoginService.ts
@@ -1,30 +1,41 @@
-import {AxiosResponse} from "axios";
+import { AxiosResponse } from "axios";
 import $api from "../http";
-import {AuthorizationResponse, AuthResponse} from "../models/response/AuthResponse";
+import {
+  AuthorizationResponse,
+  AuthResponse,
+} from "../models/response/AuthResponse";
 
 export default class AuthService {
-    static async sendCode(email: string): Promise<AxiosResponse<AuthResponse>> {
-        return $api.post('/auth/login/', { email })
-    }
+  static async sendCode(email: string): Promise<AxiosResponse<AuthResponse>> {
+    return $api.post("/auth/login/", { email });
+  }
 
-    static async postAuthorization(username: string, password: string): Promise<AxiosResponse<AuthorizationResponse>> {
-        return $api.post(`/auth/password/${username}/${password}/`)
-    }
+  static async postAuthorization(
+    username: string,
+    password: string
+  ): Promise<AxiosResponse<AuthorizationResponse>> {
+    return $api.post(`/auth/password/`, { username, password });
+  }
 
-    static async confirmEmail( codeStr: string): Promise<AxiosResponse<AuthResponse>> {
-        const code = Number(codeStr)
-        return $api.post<AuthResponse>(`/auth/token/${code}/`)
-    }
+  static async confirmEmail(
+    codeStr: string
+  ): Promise<AxiosResponse<AuthResponse>> {
+    const code = Number(codeStr);
+    return $api.post<AuthResponse>(`/auth/token/${code}/`);
+  }
 
-    static async logout() {
-        return $api.post<AuthResponse>('/auth/logout/')
-    }
+  static async logout() {
+    return $api.post<AuthResponse>("/auth/logout/");
+  }
 
-    static async putRegistration(name: string, department: number) {
-        return $api.put<AuthResponse>(`/auth/register/`, {name, department})
-    }
+  static async putRegistration(name: string, department: number) {
+    return $api.put<AuthResponse>(`/auth/register/`, { name, department });
+  }
 
-    static async postRegistration(email: string, password: string): Promise<AxiosResponse<AuthorizationResponse>> {
-        return $api.post(`/auth/passreg/${email}/${password}/`)
-    }
+  static async postRegistration(
+    email: string,
+    password: string
+  ): Promise<AxiosResponse<AuthorizationResponse>> {
+    return $api.post(`/auth/passreg/`, { username: email, password: password });
+  }
 }
