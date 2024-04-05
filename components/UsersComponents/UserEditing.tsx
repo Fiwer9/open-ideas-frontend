6f5813cf9d5ecd09a942a3e356d9aa5238b583af
SHWEDUS <krylov1243@gmail.com>
2024-04-05 20:31:42 +0500

    fix: error user editing

--- components/UsersComponents/UserEditing.tsx
+++ components/UsersComponents/UserEditing.tsx
@@ -125,7 +125,7 @@ export const UserEditing = () => {
     });
   };
 
-  const handleSaveButton = async (data: EditUserProps) => {
+  const handleSaveButton = (data: EditUserProps) => {
     const {
       department,
       superuser,
